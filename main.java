bfa6b99547c58632352ca870ecbbf36c5328d1d0
erFrankie <41636288+erFrankie@users.noreply.github.com>
2018-08-09 12:06:30 +0200

    Files

---
+++ main.java
@@ -0,0 +1,26 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+public class main {
+	public static void main(String [] args) {
+			//Este try-catch se generó automáticamente en Ventana1.java
+			/*	try {
+					Ventana1 V1 = new Ventana1();
+					V1.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			*/
+		
+		Ventana1 V1 = new Ventana1();
+		V1.setVisible(true);
+		V1.setTitle("El mejor 3 en Raya del mundo");
+
+		
+		
+		
+		
+	
+	}
+}
