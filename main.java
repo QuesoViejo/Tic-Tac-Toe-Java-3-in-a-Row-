15209ca86938fa9771abe815d06d88106e9df153
erFrankie <41636288+erFrankie@users.noreply.github.com>
2018-08-09 12:25:56 +0200

    Java and .jar files

---
+++ main.java
@@ -0,0 +1,19 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+public class main {
+	public static void main(String [] args) {
+			
+		
+		Ventana1 V1 = new Ventana1();
+		V1.setVisible(true);
+		V1.setTitle("The BEST Tic Tac Toe Game in the World");
+
+		
+		
+		
+		
+	
+	}
+}
